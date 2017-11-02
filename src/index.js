ac8dc867aceb955e2e27958f815b7dffffd590d5
Edwin Brower <edwin.brower@gmail.com>
2017-11-02 11:37:31 -0700

    set up client side

--- src/index.js
+++ src/index.js
@@ -1,2 +1,52 @@
 const board = document.querySelector('#board');
 
+const input = document.querySelector('form');
+
+const validateInput = (moveFrom, moveTo) => {
+  if (moveFrom.length !== 2 || moveTo.length !== 2) {
+    return false;
+  } 
+  if (!isNaN(Number(moveFrom[0])) || isNaN(Number(moveFrom[1])) || !isNaN(Number(moveTo[0])) || isNaN(Number(moveTo[1]))) {
+    return false;
+  } 
+  if (moveFrom[0] > 'H' || moveTo[0] > 'H') {
+    return false;
+  }
+  if (moveFrom[0] < 'A' || moveTo[0] < 'A') {
+    return false;
+  }
+  if (moveFrom[1] < '1' || moveTo[1] < '1') {
+    return false;
+  }
+  if (moveFrom[1] > '8' || moveTo[1] > '8') {
+    return false;
+  }
+  return true;
+};
+
+const processMove = e => {
+  e.preventDefault();
+  const moveFrom = e.path[0][0].value;
+  const moveTo = e.path[0][1].value;
+  input.reset();
+  console.log(moveFrom, moveTo);
+  // if (moveFrom.length !== 2 || moveTo.length !== 2 || !isNaN(Number(moveFrom[0])) || isNaN(Number(moveFrom[1])) || !isNaN(Number(moveTo[0])) || isNaN(Number(moveTo[1])) || moveFrom[0] > 'H' || moveTo[0] > 'H') {
+  if (!validateInput(moveFrom, moveTo)) {
+    window.alert(`Enter a valid input - Letter then Number - eg. A1. Your moves of '${moveFrom}' and / or '${moveTo}' did not satisfy that requirement.`);
+  } else {
+    console.log('valid');
+    // $.ajax({
+    //   url: '/move', 
+    //   type: 'POST',
+    //   data: data,
+    //   success: (data) => {
+    //     console.log('success', data);
+    //   },
+    //   error: (err) => {
+    //     console.log('err', err);
+    //   }
+    // });
+  }
+};
+
+input.addEventListener('submit', processMove);
